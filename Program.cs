16986cd0d66360f970c15d0102f605fc8813ba4c
saravanakumar204 <65958924+saravanakumar204@users.noreply.github.com>
2025-09-13 13:57:10 +0530

    Add files via upload
    Initial commit for the code changes

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using CSC_NotesService.Interface;
+using CSC_NotesService.Models;
+using CSC_NotesService.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<NotesContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
+builder.Services.AddTransient<INotesService, NotesService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
